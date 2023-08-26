08a21a10dadbd1480047fd1e608988483ba32827
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-08-26 17:22:30 +0200

    generate image form rebuild

--- app/(app)/app/(routes)/images/(routes)/generate/page.tsx
+++ app/(app)/app/(routes)/images/(routes)/generate/page.tsx
@@ -5,7 +5,7 @@ const GenarateImagePage = () => {
     <section className='container mt-16 flex flex-col items-center '>
       <div>
         <h1 className='text-3xl font-extrabold'>Generate Image</h1>
-        <p className='text-ms mt-2 text-secondary-foreground/60'>
+        <p className=' mt-2 text-secondary-foreground/60'>
           Create imaginative and visually stunning images throught
           DALL-E AI and share them with community
         </p>
--- components/images/generate-form.tsx
+++ components/images/generate-form.tsx
@@ -26,6 +26,7 @@ import { useTheme } from 'next-themes'
 import { Button } from '../ui/button'
 import axios from 'axios'
 import { toast } from '../ui/use-toast'
+import { Group, Share2 } from 'lucide-react'
 
 const generateImageFormSchema = z.object({
   prompt: z.string().min(1).max(1000),
@@ -47,8 +48,6 @@ const GenerateImageForm: React.FC = () => {
       size: '256x256',
     },
   })
-
-  console.log(photo)
 
   const onSubmit = async (data: GenerateImageFormType) => {
     try {
@@ -76,56 +75,15 @@ const GenerateImageForm: React.FC = () => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className='space-y-8 mt-16 max-w-3xl w-full'
+        className='mt-16 max-w-3xl w-full flex'
       >
-        <FormField
-          control={form.control}
-          name='prompt'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Prompt</FormLabel>
-              <FormControl>
-                <Textarea
-                  className='resize-none'
-                  placeholder='Futuristic cityscape at night with towering skyscrapers, neon lights, and flying cars, all in a cyberpunk aesthetic.'
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name='size'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Size</FormLabel>
-              <Select
-                onValueChange={field.onChange}
-                defaultValue={field.value}
-              >
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder='Select size of an image' />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value='256x256'>256x256</SelectItem>
-                  <SelectItem value='512x512'>512x512</SelectItem>
-                  <SelectItem value='1024x1024'>1024x1024</SelectItem>
-                </SelectContent>
-              </Select>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className='relative bg-secondary/30 border border-secondary text-sm rounded-lg  w-96 p-3 h-96 flex justify-center items-center'>
+        <div className='flex-shrink-0 relative bg-secondary/30 border border-secondary text-sm rounded-lg  w-96 p-3 h-96 flex justify-center items-center'>
           {photo ? (
             <Image
               alt={form.getValues('prompt')}
               src={photo}
               layout='fill'
+              className='p-2'
             />
           ) : !isGenerating ? (
             <Image
@@ -148,13 +106,76 @@ const GenerateImageForm: React.FC = () => {
             </div>
           )}
         </div>
-        <div className='mt-10 flex gap-6'>
-          <Button size='lg' type='submit' disabled={isGenerating}>
-            {isGenerating ? 'Generating...' : 'Generate image'}
-          </Button>
-          <Button variant='secondary' size='lg'>
-            Share with the community
-          </Button>
+        <div className='w-full space-y-6 ml-6 h-96 flex flex-col'>
+          <FormField
+            control={form.control}
+            name='prompt'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Prompt</FormLabel>
+                <FormControl>
+                  <Textarea
+                    className='resize-none'
+                    placeholder='Futuristic cityscape at night with towering skyscrapers, neon lights, and flying cars, all in a cyberpunk aesthetic.'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='size'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Size</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder='Select size of an image' />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value='256x256'>256x256</SelectItem>
+                    <SelectItem value='512x512'>512x512</SelectItem>
+                    <SelectItem value='1024x1024'>
+                      1024x1024
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className='flex flex-col flex-grow justify-end space-y-3'>
+            <Button
+              size='lg'
+              className='w-full'
+              type='submit'
+              disabled={isGenerating}
+            >
+              {isGenerating ? 'Generating...' : 'Generate image'}
+            </Button>
+            <div className='flex space-x-3'>
+              <Button variant='secondary' size='lg' disabled={!photo}>
+                <Group size={20} className='mr-2' />
+                Save
+              </Button>
+              <Button
+                variant='accent'
+                className='w-full'
+                size='lg'
+                disabled={!photo}
+              >
+                <Share2 className='mr-2' size={20} />
+                Share
+              </Button>
+            </div>
+          </div>
         </div>
       </form>
     </Form>
