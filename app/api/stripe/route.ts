4efab5c9bc44e29fed2db2d6cff3ba6365a4f9d6
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-10-25 15:27:09 +0200

    stripe checkout route

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,71 @@
+import { auth, currentUser } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
+import prismadb from '@/lib/prismadb'
+import { stripe } from '@/lib/stripe'
+import { absoluteUrl } from '@/lib/utils'
+
+const afterUrl = absoluteUrl('/app/dashboard')
+
+export async function GET() {
+  try {
+    const { userId } = auth()
+    const user = await currentUser()
+
+    if (!userId || !user) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const userSubscription =
+      await prismadb.userSubscription.findUnique({
+        where: { userId },
+      })
+
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      const stripeSession =
+        await stripe.billingPortal.sessions.create({
+          customer: userSubscription.stripeCustomerId,
+          return_url: afterUrl,
+        })
+
+      return new NextResponse(
+        JSON.stringify({ url: stripeSession.url }),
+      )
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: afterUrl,
+      cancel_url: afterUrl,
+      payment_method_types: ['card'],
+      mode: 'subscription',
+      billing_address_collection: 'auto',
+      customer_email: user.emailAddresses[0].emailAddress,
+      line_items: [
+        {
+          price_data: {
+            currency: 'USD',
+            product_data: {
+              name: 'BetterGPT Subscription',
+              description:
+                'Unlock the full potential of the AI. Get unlimited access to all features.',
+            },
+            unit_amount: 999,
+            recurring: {
+              interval: 'month',
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    })
+
+    return new NextResponse(
+      JSON.stringify({ url: stripeSession.url }),
+    )
+  } catch (error) {
+    console.log('[STRIPE_ERROR]', error)
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
+}
