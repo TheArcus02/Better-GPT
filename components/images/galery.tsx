55a054cb2ceededd9431e8537eb0f14d90b6dd47
TheArcus02 <m.urbaniak.inf@gmail.com>
2024-02-15 18:18:48 +0100

    images in galery are now enlargable

--- components/images/galery.tsx
+++ components/images/galery.tsx
@@ -2,14 +2,34 @@
 import { Image } from '@prisma/client'
 import ImageCard from './image-card'
 import SearchBar from './search-bar'
+import { useWindowWidth } from '@/hooks/use-window-width'
+import { useEffect, useState } from 'react'
+import { tailwindBreakpoints } from '@/lib/breakpoints'
 interface GaleryProps {
   images: Image[]
 }
 
 const Galery: React.FC<GaleryProps> = ({ images }) => {
-  const getColumns = (colIndex: number) => {
-    return images.filter((_, index) => index % 3 === colIndex)
+  const windowWidth = useWindowWidth()
+
+  const [columnsArray, setColumnsArray] = useState<Image[][]>([])
+
+  const widthToColumns = (width: number) => {
+    if (width >= tailwindBreakpoints.xl) return 3
+    if (width >= tailwindBreakpoints.md) return 2
+    return 1
   }
+
+  useEffect(() => {
+    const cols = widthToColumns(windowWidth)
+    const newColumnsArray: Image[][] = Array.from(
+      { length: cols },
+      (_, colIndex) =>
+        images.filter((_, index) => index % cols === colIndex),
+    )
+
+    setColumnsArray(newColumnsArray)
+  }, [windowWidth, images, columnsArray.length])
 
   return (
     <>
@@ -17,17 +37,15 @@ const Galery: React.FC<GaleryProps> = ({ images }) => {
         <SearchBar />
       </div>
 
-      {images.length > 0 ? (
-        <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4'>
-          {[getColumns(0), getColumns(1), getColumns(2)].map(
-            (column, index) => (
-              <div className='flex flex-col gap-4' key={index}>
-                {column.map((image) => (
-                  <ImageCard key={image.id} image={image} />
-                ))}
-              </div>
-            ),
-          )}
+      {columnsArray.length > 0 ? (
+        <div className='grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4'>
+          {columnsArray.map((column, index) => (
+            <div className='flex flex-col gap-4' key={index}>
+              {column.map((image) => (
+                <ImageCard key={image.id} image={image} />
+              ))}
+            </div>
+          ))}
         </div>
       ) : (
         <div>No results.</div>
--- components/images/image-card.tsx
+++ components/images/image-card.tsx
@@ -4,7 +4,13 @@ import React from 'react'
 import { CldImage } from 'next-cloudinary'
 import Link from 'next/link'
 import FileSaver from 'file-saver'
-import { Download, Lock, MoreVertical, Share2 } from 'lucide-react'
+import {
+  Download,
+  Expand,
+  Lock,
+  MoreVertical,
+  Share2,
+} from 'lucide-react'
 import {
   Tooltip,
   TooltipContent,
@@ -20,6 +26,7 @@ import {
   CommandItem,
   CommandList,
 } from '../ui/command'
+import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog'
 
 interface ImageCardProps {
   image: ImageSchema
@@ -33,105 +40,127 @@ const ImageCard: React.FC<ImageCardProps> = ({ image }) => {
     FileSaver.saveAs(photo, `download-${id}.jpg`)
   }
 
+  const imageWidth = parseInt(image.size.split('x')[0])
   const imageHeight = parseInt(image.size.split('x')[1])
 
   return (
-    <div className='rounded-xl group relative card max-w-fit'>
-      <CldImage
-        src={image.publicId}
-        alt={image.prompt}
-        height={imageHeight}
-        width={528}
-        crop='fill'
-        sizes='100vw, (min-width: 768px) 33vw, (min-width: 1024px) 25vw'
-      />
-      <div className=' group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-secondary m-2 p-4 rounded-md'>
-        <p className='text-foreground text-sm overflow-y-auto prompt'>
-          {image.prompt}
-        </p>
+    <Dialog>
+      <div className='rounded-xl group relative card max-w-fit'>
+        <CldImage
+          src={image.publicId}
+          alt={image.prompt}
+          height={imageHeight}
+          width={528}
+          crop='fill'
+          sizes='100vw, (min-width: 768px) 33vw, (min-width: 1024px) 25vw'
+        />
+        <div className=' group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-secondary m-2 p-4 rounded-md'>
+          <p className='text-foreground text-sm overflow-y-auto prompt'>
+            {image.prompt}
+          </p>
 
-        <div className='mt-5 flex justify-between items-center gap-2'>
-          <Link
-            href={`/app/images/${image.userId}`}
-            className='flex items-center gap-2'
-          >
-            <Avatar className='w-7 h-7'>
-              <AvatarImage src={image.profilePicture} />
-              <AvatarFallback>
-                {image.username[0].toUpperCase()}
-              </AvatarFallback>
-            </Avatar>
-            <p className='text-foreground text-sm'>
-              {image.username}
-            </p>
-          </Link>
-          <div className={isOwner ? 'flex gap-2' : ''}>
-            <Tooltip>
-              <TooltipTrigger>
-                <button
-                  type='button'
-                  onClick={() => downloadImage(image.id, image.url)}
-                  className='outline-none bg-transparent border-none'
-                >
-                  <Download className='w-6 h-6' size={24} />
-                </button>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p className='text-foreground text-sm'>Download</p>
-              </TooltipContent>
-            </Tooltip>
-
-            {isOwner && (
+          <div className='mt-5 flex justify-between items-center gap-2'>
+            <Link
+              href={`/app/images/${image.userId}`}
+              className='flex items-center gap-2'
+            >
+              <Avatar className='w-7 h-7'>
+                <AvatarImage src={image.profilePicture} />
+                <AvatarFallback>
+                  {image.username[0].toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+              <p className='text-foreground text-sm'>
+                {image.username}
+              </p>
+            </Link>
+            <div className={isOwner ? 'flex gap-2' : ''}>
               <Tooltip>
                 <TooltipTrigger>
-                  <Popover>
-                    <PopoverTrigger>
-                      <button
-                        type='button'
-                        className='outline-none bg-transparent border-none'
-                      >
-                        <MoreVertical className='w-6 h-6' size={24} />
-                      </button>
-                    </PopoverTrigger>
-                    <PopoverContent className='w-52 px-1 z-10'>
-                      <Command>
-                        <CommandList>
-                          <CommandGroup>
-                            {image.shared ? (
-                              <CommandItem className='cursor-pointer'>
-                                <Lock className='mr-2 h-4 w-4' />
-                                <span>Make Private</span>
-                              </CommandItem>
-                            ) : (
-                              <CommandItem className='cursor-pointer'>
-                                <Share2 className='mr-2 h-4 w-4' />
-                                <span>Share</span>
-                              </CommandItem>
-                            )}
-                            <CommandItem
-                              className='cursor-pointer'
-                              onClick={() =>
-                                downloadImage(image.id, image.url)
-                              }
-                            >
-                              <Download className='mr-2 w-4 h-4' />
-                              <span>Download</span>
-                            </CommandItem>
-                          </CommandGroup>
-                        </CommandList>
-                      </Command>
-                    </PopoverContent>
-                  </Popover>
+                  <button
+                    type='button'
+                    onClick={() => downloadImage(image.id, image.url)}
+                    className='outline-none bg-transparent border-none'
+                  >
+                    <Download className='w-6 h-6' size={24} />
+                  </button>
                 </TooltipTrigger>
                 <TooltipContent>
-                  <p className='text-foreground text-sm'>Actions</p>
+                  <p className='text-foreground text-sm'>Download</p>
                 </TooltipContent>
               </Tooltip>
-            )}
+
+              {isOwner && (
+                <Tooltip>
+                  <TooltipTrigger>
+                    <Popover>
+                      <PopoverTrigger>
+                        <button
+                          type='button'
+                          className='outline-none bg-transparent border-none'
+                        >
+                          <MoreVertical
+                            className='w-6 h-6'
+                            size={24}
+                          />
+                        </button>
+                      </PopoverTrigger>
+                      <PopoverContent className='w-52 px-1 z-10'>
+                        <Command>
+                          <CommandList>
+                            <CommandGroup>
+                              <DialogTrigger className='w-full'>
+                                <CommandItem className='cursor-pointer'>
+                                  <Expand className='mr-2 h-4 w-4' />
+                                  <span>Enlarge</span>
+                                </CommandItem>
+                              </DialogTrigger>
+                              <DialogContent className='max-w-[90vw] max-h-[90vh] md:w-max'>
+                                <CldImage
+                                  src={image.publicId}
+                                  alt={image.prompt}
+                                  height={imageHeight}
+                                  width={imageWidth}
+                                  className='object-scale-down max-h-[80vh]'
+                                />
+                              </DialogContent>
+
+                              {image.shared ? (
+                                <CommandItem className='cursor-pointer'>
+                                  <Lock className='mr-2 h-4 w-4' />
+                                  <span>Make Private</span>
+                                </CommandItem>
+                              ) : (
+                                <CommandItem className='cursor-pointer'>
+                                  <Share2 className='mr-2 h-4 w-4' />
+                                  <span>Share</span>
+                                </CommandItem>
+                              )}
+                              <CommandItem
+                                className='cursor-pointer'
+                                onClick={() =>
+                                  downloadImage(image.id, image.url)
+                                }
+                              >
+                                <Download className='mr-2 w-4 h-4' />
+                                <span>Download</span>
+                              </CommandItem>
+                            </CommandGroup>
+                          </CommandList>
+                        </Command>
+                      </PopoverContent>
+                    </Popover>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p className='text-foreground text-sm'>Actions</p>
+                  </TooltipContent>
+                </Tooltip>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Dialog>
   )
 }
 
